6c5889c0801961882ee19fbd23393c04ae7ecefd
Mian Akbar Jan <mianakbarjan@hotmail.com>
2023-05-09 19:11:33 +0500

    Post Ad Implemented

--- src/pages/PostAd.jsx
+++ src/pages/PostAd.jsx
@@ -1,7 +1,31 @@
 import React,{useState} from "react";
 import ConfirmationPopUp from "../components/ConfirmationPopUp";
+import axios from 'axios'
+import Cookies from 'universal-cookie'
+
+const cookies = new Cookies()
+
+
 const PostAd = () => {
     const [postAdBtn, setPostAdBtn] = useState(false)
+
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [price, setPrice] = useState("")
+    const userid = cookies.get("token").id
+    console.log(userid)
+
+    const postAd = async (e) =>{
+      e.preventDefault()
+      await axios.post('http://localhost:4500/ad/post', {
+        title, description, price, userid
+      })
+      .then (() => {
+        window.location.href = '/homepage';
+      })
+    }
+  
+
   return (
 
     <div className="w-full bg-emerald-400 pt-24 pb-32">
@@ -13,6 +37,7 @@ const PostAd = () => {
           <input
             className="my-2 p-2 border-2 border-emerald-500 "
             type="text"
+            onChange={e => setTitle(e.target.value)} required
           />
           <label className="my-2 text-emerald-600 font-bold text-xl" htmlFor="">
             Description
@@ -20,6 +45,7 @@ const PostAd = () => {
           <textarea
             className="my-2 p-2 border-2 border-emerald-500 h-80"
             type="text-Area"
+            onChange={e => setDescription(e.target.value)} required
           ></textarea>
           <label className="my-2 text-emerald-600 font-bold text-xl" htmlFor="">
             Attach images
@@ -38,11 +64,17 @@ const PostAd = () => {
             <input
               className="my-2 mx-4 px-2 border-2 border-emerald-500 inline-block w-20"
               type="text"
+            onChange={e => setPrice(e.target.value)} required
             />
             <span className="text-emerald-500 font-bold">Rupees</span>
           </div>
         </form>
-          <button onClick={()=>{setPostAdBtn(true)}} className="mx-auto block bg-emerald-400 rounded-full py-2 px-8 font-bold text-white mt-10 hover:-translate-y-1 transition-transform active:translate-y-1">
+          {/* <button onClick={()=>{setPostAdBtn(true)}} className="mx-auto block bg-emerald-400 rounded-full py-2 px-8 font-bold text-white mt-10 hover:-translate-y-1 transition-transform active:translate-y-1">
+            Post Ad
+          </button> */}
+          {/* fix this part */}
+          
+          <button onClick={postAd} className="mx-auto block bg-emerald-400 rounded-full py-2 px-8 font-bold text-white mt-10 hover:-translate-y-1 transition-transform active:translate-y-1">
             Post Ad
           </button>
       </div>
